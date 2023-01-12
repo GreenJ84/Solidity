a2eb939ddf32b0b22a70cf2f5f50e51ade45d454
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-12 01:57:48 -0800

    Updated Crypto Lottery contract

--- Crypto_Lottery/Lottery_Contract/contracts/CryptoLottery.sol
+++ Crypto_Lottery/Lottery_Contract/contracts/CryptoLottery.sol
@@ -2,8 +2,10 @@
 pragma solidity 0.8.17;
 
 contract CryptoLottery {
+  event WinningAccount(address _winner);
+
   address public manager;
-  uint public entryFee;
+  uint public entryFee = 0;
   address payable[] public participants;
 
 
@@ -11,36 +13,46 @@ contract CryptoLottery {
     manager = msg.sender;
   }
 
+  // Manager Functions Set Pool, Pick Winner(random), and Pay Winner
   modifier onlyManager() {
         require(msg.sender == manager, "You are not the manager");
         _;
   }
-
-  function setEntry(uint amt) public onlyManager{
-    entryFee = amt;
+  function setEntry() public payable onlyManager{
+    require(entryFee==0, "Cannot reset entry fee during an open session");
+    require(msg.value>=1000000000000000, "Entry Fee must be over 1 finney");
+    entryFee = msg.value;
+    participants.push(payable(msg.sender));
+  }
+  function drawWinner() public onlyManager{
+    uint rand = random();
+    address payable winner = participants[rand];
+    emit WinningAccount(winner);
+    payWinner(winner);
+    participants = new address payable[](0);
+  }
+  function payWinner(address payable _winner) public onlyManager{
+    require(_winner.send(address(this).balance));
   }
 
   modifier validEntry() {
+    require(participants.length>0, "Session not started for the next pool.");
     require(msg.value > entryFee, "Not enough for even 1 entry");
     _;
   }
   function newEntry() public payable validEntry{
-    participants.push(payable(msg.sender));
+    uint x = msg.value/entryFee;
+    uint i;
+    for(i = 0; i < x; i++){
+        participants.push(payable(msg.sender));
+    }
   }
 
 
   function random() public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants)))%participants.length;
-    }
-
-  function drawWinner() public onlyManager{
-    uint rand = random();
-    address payable winner = participants[rand];
-    payWinner(winner);
-    participants = new address payable[](0);
+    return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, participants)))%participants.length;
   }
-
-  function payWinner(address payable _winner) public onlyManager{
-    require(_winner.send(address(this).balance));
+  function getParticipants() public view returns(uint){
+    return participants.length;
   }
 }
