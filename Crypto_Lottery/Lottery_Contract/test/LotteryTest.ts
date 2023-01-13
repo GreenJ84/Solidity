985da5439928ada2a0092f1ae3b607eac313f391
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-13 15:47:36 -0800

    Format test file

--- Crypto_Lottery/Lottery_Contract/test/LotteryTest.ts
+++ Crypto_Lottery/Lottery_Contract/test/LotteryTest.ts
@@ -1,69 +1,62 @@
-import { CryptoLotteryContract } from "../types/truffle-contracts";
+/** @format */
 
-const CryptoLottery = artifacts.require('CryptoLottery');
+const CryptoLottery = artifacts.require("CryptoLottery");
 
-console.log('testing');
+console.log("testing");
 contract("CryptoLottery", async (accounts: string[]) => {
-    let lottery: any;
-    before("", async () => {
-        lottery = await CryptoLottery.deployed()
-    })
+  let lottery: any;
+  before("", async () => {
+    lottery = await CryptoLottery.deployed();
+  });
 
-    it("The account deploying should be manager", async () => {
-        let manager = await lottery.manager();
-        assert.equal(manager, accounts[0]);
-    });
+  it("The account deploying should be manager", async () => {
+    let manager = await lottery.manager();
+    assert.equal(manager, accounts[0]);
+  });
 
-    it("No account except manager should set the pot", async () => {
-        try {
-            await lottery.setEntry({ from: accounts[1], value: "10" })
-            assert(false)
-        } catch {
-            assert(true)
-        }
-    });
+  it("No account except manager should set the pot", async () => {
+    try {
+      await lottery.setEntry({ from: accounts[1], value: "10" });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
 
-    it("Noone should be able to call Pay winner", async () => {
-        try {
-            await lottery.drawWinner({ from: accounts[1] });
-            assert(false);
-        }
-        catch {
-            assert(true);
-        }
-    });
+  it("Noone should be able to call Pay winner", async () => {
+    try {
+      await lottery.drawWinner({ from: accounts[1] });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
 
-    it("Noone should be able to call Draw winner", async () => {
-        try {
-            await lottery.payWinner(accounts[1], { from: accounts[1] });
-            assert(false);
-        }
-        catch {
-            assert(true);
-        }
-    });
+  it("Noone should be able to call Draw winner", async () => {
+    try {
+      await lottery.payWinner(accounts[1], { from: accounts[1] });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
 
-    it("Users cannot join until Fee is set", async () => {
-        try {
-            await lottery.newEntry({ from: accounts[1], value: "2" });
-            assert(false);
-        }
-        catch {
-            assert(true);
-        }
-    });
+  it("Users cannot join until Fee is set", async () => {
+    try {
+      await lottery.newEntry({ from: accounts[1], value: "2" });
+      assert(false);
+    } catch {
+      assert(true);
+    }
+  });
 
-    it("Manager can set entry and User can enter", async () => {
-        try {
-            await lottery.setEntry({ from: accounts[0], value: "1000000000000000" })
-            await lottery.newEntry({ from: accounts[1], value: "5000000000000000" });
-            assert(true);
-        }
-        catch {
-            assert(false);
-        }
-
-
-    });
-
+  it("Manager can set entry and User can enter", async () => {
+    try {
+      await lottery.setEntry({ from: accounts[0], value: "1000000000000000" });
+      await lottery.newEntry({ from: accounts[1], value: "5000000000000000" });
+      assert(true);
+    } catch {
+      assert(false);
+    }
+  });
 });
