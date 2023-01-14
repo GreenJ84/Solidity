e6d3373be80e25f113274a8c45f804274a0c6399
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 04:02:19 -0800

    Import and render Input Modal

--- nft-galary/src/pages/index.tsx
+++ nft-galary/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React from "react"
+import InputModal from "../components/InputModal";
+
 
 export default function Home() {
+
   return (
     <>
-      
+      <InputModal />
+
     </>
   )
 }
