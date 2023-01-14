94516a1081ad86b37899a835885ecab2f599acea
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 14:03:02 -0800

    Updated image path routes for next config

--- nft-galary/next.config.js
+++ nft-galary/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "i.seadn.io",
+      "nft-cdn.alchemy.com",
+      "imgs.search.brave.com"
+    ]
+  }
 }
 
 module.exports = nextConfig
