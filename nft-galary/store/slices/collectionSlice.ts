fa0ccb79173712fa655b5f5c203880526842c063
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 02:56:50 -0800

    Created redux slice to manage collection state

---
+++ nft-galary/store/slices/collectionSlice.ts
@@ -0,0 +1,22 @@
+/** @format */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface collectionType {
+  value: string;
+}
+
+const collectionSlice = createSlice({
+  name: "collection",
+  initialState: {
+    value: "",
+  } as collectionType,
+  reducers: {
+    setCollection: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const {} = collectionSlice.actions;
+export default collectionSlice.reducer;
