9343277492ede9abad70df9e5571953317c5030d
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 14:04:13 -0800

    Updated GalaryBody imports and structure

--- nft-galary/src/components/GalaryBody.tsx
+++ nft-galary/src/components/GalaryBody.tsx
@@ -1,20 +1,21 @@
+import { nftsType } from '@/pages'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { storeType } from 'store/store'
-import ImageBoard from './ImageBoard'
+import GalaryDisplay from './GalaryDisplay'
 
-import { nftData } from '@/pages/index'
 
-const GalaryBody = (props: { nfts: Array<nftData> }) => {
+
+const GalaryBody = (props: { nfts: Array<nftsType>}) => {
   const { account, collection } = useSelector((state: storeType) => state)
   return (
     <>
       <h1 className="title"> Currently displaying NFTs from
-        {account.value ? <p> Account address <span>{account.value}</span></p> : ""}
+        {account.value ? <p> Account address: <span>{account.value}</span></p> : ""}
         { account.value && collection.value ? <p> and </p>:""}
         { collection.value ? <p> Collection address <span >{collection.value}</span></p>:""}
       </h1>
-      <ImageBoard nfts={props.nfts}/>
+      <GalaryDisplay nfts={props.nfts}/>
     </>
   )
 }
