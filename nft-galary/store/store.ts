05544353cccaa99c885f0a14d30832f9d8f8cb13
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 03:03:22 -0800

    created application store

---
+++ nft-galary/store/store.ts
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit'
+import accountReducer, { accountType } from './slices/accountSlice'
+import collectionReducer, { collectionType } from './slices/collectionSlice'
+
+export interface storeType {
+    account: accountType
+    collection: collectionType
+}
+
+const store = configureStore({
+    reducer: {
+        account: accountReducer,
+        collection: collectionReducer,
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
