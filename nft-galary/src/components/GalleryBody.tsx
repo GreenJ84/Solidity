cbd95993093cbfc2ea7e25b34f55ed39d6f30041
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 16:57:31 -0800

    Updated imports, renders store values

--- nft-galary/src/components/GalleryBody.tsx
+++ nft-galary/src/components/GalleryBody.tsx
@@ -2,20 +2,23 @@ import { nftsType } from '@/pages'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { storeType } from 'store/store'
-import GalaryDisplay from './GalaryDisplay'
+import GalleryDisplay from './GalleryDisplay'
 
 
 
 const GalaryBody = (props: { nfts: Array<nftsType>}) => {
   const { account, collection } = useSelector((state: storeType) => state)
+  
   return (
     <>
       <h1 className="title"> Currently displaying NFTs from
-        {account.value ? <p> Account address: <span>{account.value}</span></p> : ""}
+        {account.value ? <p> Account address:
+          <span>{account.value}</span></p> : ""}
         { account.value && collection.value ? <p> and </p>:""}
-        { collection.value ? <p> Collection address <span >{collection.value}</span></p>:""}
+        {collection.value ? <p> Collection address: 
+          <span >{collection.value}</span></p> : ""}
       </h1>
-      <GalaryDisplay nfts={props.nfts}/>
+      <GalleryDisplay nfts={props.nfts}/>
     </>
   )
 }
