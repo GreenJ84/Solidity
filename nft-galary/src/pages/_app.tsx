36e336dfe8d2e973219b8981b9b97773897eae30
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 16:59:29 -0800

    imported Nft Display stylesheet

--- nft-galary/src/pages/_app.tsx
+++ nft-galary/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/InputModal.css'
+import '@/styles/nftDisplay.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from '../../store/store'
