04ad7d2b6b7daf291596c148746b4ec34ee261df
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-14 17:00:13 -0800

    Updated conditional rendering and interface

--- nft-galary/src/pages/index.tsx
+++ nft-galary/src/pages/index.tsx
@@ -1,65 +1,105 @@
 /** @format */
 
+import InputModal from "@/components/InputModal";
 import { GetServerSideProps } from "next";
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { storeType } from "store/store";
 
-import GalaryBody from "../components/GalaryBody";
-import InputModal from "../components/InputModal";
+import GalleryBody from "../components/GalleryBody";
 
-export interface nftData {
-  nfts: Object[];
-  totalCount?: number;
+export interface nftsType{
+  title: string
+  description: string
+  media: [
+    {
+      gateway: string
+    }
+  ]
+  id: {
+    tokenId: string
+  }
+  contract: {
+    address: string
+  }
 }
 
 
-export default function Home(props: { apikey: string }) {
+export default function Home(props: { apikey: string, nfts: Array<nftsType> }) {
   const api_key = props.apikey
-  const [nfts, setNfts] = useState({});
+  const [nfts, setNfts] = useState(props.nfts);
   const { account, collection } = useSelector((state: storeType) => state);
-
-  console.log(api_key);
   
   const fetchNFTs = async () => {
-    let _nfts;
-    console.log("Fetching NFTs");
-    console.log(api_key, account.value, collection.value)
+    if (account.value == "") {
+      return
+    }
     let fetchURL;
     const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${api_key}/getNFTs/`;
     const requestOptions = {
       method: 'GET'
     }
-    
-    if (!collection.value) {
+
+    if (collection.value == "") {
       fetchURL = `${baseURL}?owner=${account.value}`
     } else {
       fetchURL = `${baseURL}?owner=${account.value}&contractAddresses%5B%5D=${collection.value}`
     }
     console.log(fetchURL);
-    _nfts = await fetch(fetchURL, requestOptions)
-    .then(data => data.json())
-    
+
+    console.log("-- Fetching NFTs --");
+    const _nfts = await fetch(fetchURL, requestOptions)
+      .then(data => data.json());
+
     if (_nfts) {
-      console.log(_nfts)
-      setNfts(_nfts)
+      setNfts(_nfts.ownedNfts);
     }
   }
-  const fetchCollection = async () => {};
+
+
+  const fetchCollection = async () => {
+    if (!collection.value) {
+      return
+    }
+    const baseUrl: string = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
+    const requestOptions = {
+      method: "GET"
+    }
+
+    const fetchUrl = `${baseUrl}?contractAddress=${collection.value}&withMetadata=true`;
+    console.log(fetchUrl);
+
+    console.log("-- Fetching NFTs --");
+    const _nfts = await fetch(fetchUrl, requestOptions)
+      .then(data => data.json());
+    if (_nfts) {
+      setNfts(_nfts.nfts);
+    }
+
+  };
 
   return (
     <>
       <InputModal fetchA={() => fetchNFTs()} fetchC={() => fetchCollection()} />
-      <GalaryBody nfts={nfts} />
+      <GalleryBody nfts={nfts} />
     </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const api_key = process.env.REACT_APP_API_KEY;
+  const Url = `https://eth-mainnet.g.alchemy.com/nft/v2/${api_key}/getNFTs/?owner=0x2219772388c4CCcCB8E5D71197965cee1B124622`;
+  const requestOptions = {
+    method: "GET"
+  }
+
+  const _nfts = await fetch(Url, requestOptions)
+    .then(data => data.json())
+
   return {
     props: {
-      apikey: api_key
+      apikey: api_key,
+      nfts: _nfts.ownedNfts
     }
   }
 }
