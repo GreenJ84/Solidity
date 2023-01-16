cba477085623bc79fed2d33478fa9dacf7c86000
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-16 10:00:53 -0800

    Updated hardhat config env conditional

--- PolygonNFT/hardhat.config.ts
+++ PolygonNFT/hardhat.config.ts
@@ -2,17 +2,13 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 require("dotenv").config();
 
-let TESTNET_RPC: string;
-let PRIVATE_KEY: string;
-let POLYGONSCAN_API_KEY: string;
 
-if (process.env.PRIVATE_KEY && process.env.TESTNET_RPC && process.env.POLYGONSCAN_API_KEY) {
-  PRIVATE_KEY = process.env.PRIVATE_KEY;
-  TESTNET_RPC = process.env.TESTNET_RPC;
-  POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
-} else {
+if (!process.env.PRIVATE_KEY || !process.env.TESTNET_RPC || !process.env.POLYGONSCAN_API_KEY) {
   throw new Error("PRIVATE_KEY, TESTNET_RPC, and POLYGONSCAN_API_KEY are require! ")
 }
+const TESTNET_RPC: string = process.env.TESTNET_RPC
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY;
+const POLYGONSCAN_API_KEY: string = process.env.POLYGONSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
