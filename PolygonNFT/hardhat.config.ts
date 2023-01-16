99f312a3f8b09d7a27bb08c53991fdb0b4d8d88b
Jesse Greenough <jesse.greenough84@gmail.com>
2023-01-15 19:47:16 -0800

    Uodated hardhat config networks with env variables

--- PolygonNFT/hardhat.config.ts
+++ PolygonNFT/hardhat.config.ts
@@ -1,8 +1,30 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
+
+let TESTNET_RPC: string;
+let PRIVATE_KEY: string;
+let POLYGONSCAN_API_KEY: string;
+
+if (process.env.PRIVATE_KEY && process.env.TESTNET_RPC && process.env.POLYGONSCAN_API_KEY) {
+  PRIVATE_KEY = process.env.PRIVATE_KEY;
+  TESTNET_RPC = process.env.TESTNET_RPC;
+  POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
+} else {
+  throw new Error("PRIVATE_KEY, TESTNET_RPC, and POLYGONSCAN_API_KEY are require! ")
+}
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: TESTNET_RPC,
+      accounts: [ PRIVATE_KEY ]
+    },
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY
+  }
 };
 
 export default config;
